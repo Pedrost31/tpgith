7b4bdfbfadc0c3891861730c34c06b9d8b3887fb
Pedro <m.dossantosataide@ecoles-epsi.net>
2024-01-31 13:17:59 +0100

    Sur hello.py, afficher les multiples de 1 à 10 du nombre entré

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
 nombre = input("Veuillez saisir un nombre : ")
-
 print("Le nombre que vous avez saisi est :", nombre)
+
+print("Les multiples de", nombre, "sont :")
+for i in range(1, 11):
+    multiple = nombre * i
+    print(nombre, "x", i, "=", multiple)
