38b1b47b47c0460811bc981aec7426df05ca19b7
Pedro <m.dossantosataide@ecoles-epsi.net>
2024-01-31 13:24:51 +0100

    Modification pour demander à la fin si l'utilisateur souahite terminer le programme ou recommencer

--- hello.py
+++ hello.py
@@ -5,3 +5,23 @@ print("Les multiples de", nombre, "sont :")
 for i in range(1, 11):
     multiple = nombre * i
     print(nombre, "x", i, "=", multiple)
+
+while True:
+
+    nombre = input("Veuillez saisir un nombre : ")
+    print("Le nombre que vous avez saisi est :", nombre)
+
+    print("Les multiples de", nombre, "sont :")
+    for i in range(1, 11):
+        multiple = int(nombre) * i  
+        print(nombre, "x", i, "=", multiple)
+    
+
+    reponse = input("Voulez-vous recommencer le programme ? (O/N): ").strip().upper()
+    
+    if reponse != "O":
+        print("Le programme est terminé.")
+        break
+
+
+   
